4ff10c01c76f99a44895a896cc68f55fca106102
yashshinde-dev01 <shindeyash.dev@gmail.com>
2026-03-17 22:44:56 +0530

    Add files via upload

---
+++ 02_DataTypes.js
@@ -0,0 +1,20 @@
+"use strict";// treat all js code as newer version
+console.log(4+3);
+let name="Hitesh"
+let age=90
+let isLogges=false
+
+// number ==> range 2 poer 53
+//bigint
+// string ==""or ''
+//boolean => false/true
+// null ==> standalone value 
+// undefined ==> 
+// symbol==> unique 
+
+
+//object 
+
+console.log(typeof undefined)
+
+
