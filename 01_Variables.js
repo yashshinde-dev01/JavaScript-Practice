a027df0cc63354ed34c9905c24d1e970c0f63279
yashshinde-dev01 <shindeyash.dev@gmail.com>
2026-03-17 22:44:15 +0530

    Add files via upload
    variables in js

---
+++ 01_Variables.js
@@ -0,0 +1,17 @@
+const accountId=12223;
+let accountEmail="hitesh@gmail.com"
+var accountPassword="124"
+accountCity="jaipur"
+let accountstate;
+// accountId=2 not allowed
+accountEmail="hc@gamil"
+accountCity="mumbai"
+console.log(accountId);
+console.table([accountEmail,accountId,accountCity,accountstate]);
+/*
+prefer not to use var
+beacuse isssue in block scope
+*/
+
+
+ 
