4ddd3f3d4068b9524392df8aea0bf10b70456297
yashshinde-dev01 <shindeyash.dev@gmail.com>
2026-03-22 10:02:07 +0530

    Enhance 02_DataTypes.js with type conversion examples and  operations
    Added examples of type conversion and operations in JavaScript.

--- 02_DataTypes.js
+++ 02_DataTypes.js
@@ -16,5 +16,57 @@ let isLogges=false
 //object 
 
 console.log(typeof undefined)
+let score="123";
+let ValueNumber=Number(score);
+console.log(ValueNumber);
+console.log(typeof ValueNumber);
 
+// "33"==> 33
+// "33abd"==>NaN
+
+let isLoggedIn="";
+let booleanIsLoogedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoogedIn)
+
+// 1==>true
+//0==>false
+//""==>false
+//"hitesh"==>true;
+
+let someNumber=33
+let string=String(someNumber)
+console.log(string)
+console.log(typeof string)
+
+
+//****************Operations***************8******** */
+
+let value=3;
+let negVal=-value;
+// console.log(negVal)
+
+// console.log(2+2)
+// console.log(2*2)
+// console.log(3/3)
+
+// let str1="hello"
+// let str2=" hitesh"
+// let str3=str1+str2;
+// console.log(str3)
+
+// console.log("1"+4+3)//143
+// console.log(1+3+'4')//44
+
+console.log(+true)//1
+console.log(+"")//0
+
+// let  num1,num2,num3
+// num1=num2=num3=2+2
+// let gameCounter=100
+// ++gameCounter;
+// console.log(gameCounter)
+
+let x=3
+const y=++x
+console.log(`x:${x},y:${y}`)
 
