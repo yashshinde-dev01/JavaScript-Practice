bd8c16073aeae6bd79badfd438165d7a301c7a9e
yashshinde-dev01 <shindeyash.dev@gmail.com>
2026-03-22 10:20:18 +0530

    Comparision operators in js how work and Basic Understanding

---
+++ 04_Comparision.js
@@ -0,0 +1,24 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(3<4)
+
+
+// console.log("2">1)
+// console.log("02">1)
+
+
+
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0) ;// true
+
+console.log(undefined==0);
+
+// avoid above null commaprision 
+//===
+
+console.log("2"===2);
+
+
+
+
