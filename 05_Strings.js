afcd636f857f1dad2ca575338cf356d8af89c968
yashshinde-dev01 <shindeyash.dev@gmail.com>
2026-03-27 16:25:19 +0530

    Strings in js

---
+++ 05_Strings.js
@@ -0,0 +1,31 @@
+const name='hitesh'
+console.log(`hello my name is ${name}`)
+
+const  newGame=new String("Pubg-game")
+console.log(newGame)
+
+console.log(newGame[1]);
+console.log(newGame.__proto__)
+
+console.log(newGame.length);
+console.log(newGame.toUpperCase());
+console.log(newGame.charAt(3));
+
+console.log(newGame.indexOf('b'));
+
+const newString=newGame.substring(0,5)
+console.log(newString);
+
+const anotherString=newGame.slice(-7,4)
+console.log(anotherString);
+
+const newStr1="  hitesh  sir  is legend"
+console.log(newStr1);
+console.log(newStr1.split());
+console.log(newStr1.trim());
+
+
+
+
+
+
