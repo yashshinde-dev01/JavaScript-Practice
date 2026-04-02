047ccd694066909fbe97bb05a8b199cbd41416ff
yashshinde-dev01 <shindeyash.dev@gmail.com>
2026-04-02 11:57:25 +0530

    Nums and Math in JS

---
+++ 06_Nums_and_Maths.js
@@ -0,0 +1,34 @@
+// const score=2000;
+
+// const balance=new Number(12334)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+
+// const otherNumber=123.866;
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds=100000;
+// console.log(hundreds.toLocaleString('en-In'))
+
+
+//++++++++ MATHS  ++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))// abs ==>absolute
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.3))
+// console.log(Math.min(3,6))
+
+console.log(Math.random())// always come value in betwwen 0 and 1 
+console.log(Math.floor((Math.random()*10)+1))
+
+const min=10;
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
